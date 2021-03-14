dbeb3e0fb011037593c9a3142ac5870457abf96c
EC2 Default User <ec2-user@ip-172-31-31-32.us-east-2.compute.internal>
2021-03-14 21:09:47 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,17 @@
 //LP獲得用
 Route::get('live1', function () {
     return view('yt.check'); //仮LP
-    //リダイレクトLPへ切り替え - cccheckを変更
+    // return view('yt.line'); //獲得用 - LINE
     
     // return redirect('https://a-trade.jp/redirect/mocom-mail?media=G16531'); //モコム(メアド) - Aトレード
 });
 
 //OS識別→案件遷移 - LINE友達追加
-Route::get('cccheck', 'AdController@os');
+Route::get('os', 'AdController@os');
 
-// //LP確認用
-// Route::get('cccheck', function () {
-//     return redirect('https://a-trade.jp/redirect/mocom-mail?media=G16531'); //モコム(メアド) - Aトレード
-// });
+//LP確認用
+Route::get('cccheck', function () {
+    return view('yt.line'); //獲得用 - LINE
+    
+    // return redirect('https://a-trade.jp/redirect/mocom-mail?media=G16531'); //モコム(メアド) - Aトレード
+});
